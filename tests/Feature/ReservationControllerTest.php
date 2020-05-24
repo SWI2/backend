afaf0a15b6254890e647812bae93b3ea3017406d
robertoravec141 <roravec141@gmail.com>
2020-05-24 07:50:46 +0200

    Added test for billing upon reservation create

--- tests/Feature/ReservationControllerTest.php
+++ tests/Feature/ReservationControllerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Tests\SeededTest;
 use App\Reservation;
+use App\Customer;
+use Illuminate\Support\Facades\Storage;
 
 class ReservationControllerTest extends SeededTest
 {
@@ -14,8 +16,8 @@ class ReservationControllerTest extends SeededTest
         $this->withoutExceptionHandling();
 
         $response = $this->post('api/reservation', [
-            'name' => 'Robo Oravec',
-            'email' => 'roravec141@gmail.com',
+            'name' => 'Example',
+            'email' => 'example@email.com',
             'phone' => '+420123456789',
             'from' => '2020-03-06T19:30Z',
             'to' => '2020-03-06T19:30Z',
@@ -24,5 +26,29 @@ class ReservationControllerTest extends SeededTest
         ]);
 
         $response->assertStatus(201);
+    }
+
+    /** @test*/
+    public function reservation_advance_billing_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $note = uniqid();
+        $this->post('api/reservation', [
+            'name' => 'Example',
+            'email' => 'example@email.com',
+            'phone' => '+420123456789',
+            'from' => '2020-03-06T19:30Z',
+            'to' => '2020-03-06T19:30Z',
+            'note' => $note,
+            'car_id' => 1
+        ]);
+
+        $reservation = Reservation::where('note', $note)->first();
+
+        $path = '/reservations/'.$reservation->id.'/AdvanceBilling.pdf';
+        $fileExists = Storage::disk('local')->exists($path);
+
+        $this->assertTrue($fileExists);
     }
 }
