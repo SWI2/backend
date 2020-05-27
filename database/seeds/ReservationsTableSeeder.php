d2213119d01dd4467754959d1fc8f6be1cfc8d59
robertoravec141 <roravec141@gmail.com>
2020-05-27 07:59:49 +0200

    Seed of files moved to file seeder

--- database/seeds/ReservationsTableSeeder.php
+++ database/seeds/ReservationsTableSeeder.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Seeder;
 use App\Reservation;
-use App\FileGenerators\ReservationAdvanceBillingFileGenerator;
 
 class ReservationsTableSeeder extends Seeder
 {
@@ -14,12 +13,5 @@ class ReservationsTableSeeder extends Seeder
     public function run()
     {   
         factory(Reservation::class, 3)->create();
-        $reservations = Reservation::all();
-        foreach ($reservations as $reservation) {
-            $generator = new ReservationAdvanceBillingFileGenerator($reservation);
-            $file = $generator->generateFile();
-            $file->reservation()->associate($reservation);
-            $file->save();
-        }
     }
 }
